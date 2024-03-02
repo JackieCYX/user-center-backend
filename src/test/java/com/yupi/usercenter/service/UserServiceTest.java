0c8b5c12866ed72d88c4bdf6856e2355b0c8c94a
JordanCYX <2338257659@qq.com>
2024-03-02 14:42:47 +0800

    测试类完善

--- src/test/java/com/yupi/usercenter/service/UserServiceTest.java
+++ src/test/java/com/yupi/usercenter/service/UserServiceTest.java
@@ -3,6 +3,7 @@ import java.util.Date;
 
 import com.yupi.usercenter.model.domain.User;
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -19,27 +20,92 @@ class UserServiceTest {
     @Resource
     private UserService userService;
 
+    /**
+     * 测试添加用户
+     */
     @Test
     public void testAddUser() {
         User user = new User();
-        user.setUsername("123");
-        user.setUserAccount("512351");
-        user.setAvatarUrl("34134");
+        user.setUsername("dogYupi");
+        user.setUserAccount("123");
+        user.setAvatarUrl("https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/img/logo.png");
         user.setGender(0);
-        user.setUserPassword("123456");
-        user.setPhone("234234");
-        user.setEmail("52352");
+        user.setUserPassword("xxx");
+        user.setPhone("123");
+        user.setEmail("456");
         boolean result = userService.save(user);
         System.out.println(user.getId());
-        Assert.assertTrue(result);
+        Assertions.assertTrue(result);
     }
 
+    /**
+     * 测试更新用户
+     */
+    @Test
+    public void testUpdateUser() {
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("dogYupi");
+        user.setUserAccount("123");
+        user.setAvatarUrl("https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/img/logo.png");
+        user.setGender(0);
+        user.setUserPassword("xxx");
+        user.setPhone("123");
+        user.setEmail("456");
+        boolean result = userService.updateById(user);
+        Assertions.assertTrue(result);
+    }
+
+    /**
+     * 测试删除用户
+     */
+    @Test
+    public void testDeleteUser() {
+        boolean result = userService.removeById(1L);
+        Assertions.assertTrue(result);
+    }
+
+    /**
+     * 测试获取用户
+     */
+    @Test
+    public void testGetUser() {
+        User user = userService.getById(1L);
+        Assertions.assertNotNull(user);
+    }
+
+    /**
+     * 测试用户注册
+     */
     @Test
     void userRegister() {
-        String userAccount = "Kallin.chen";
-        String userPassword = "123456";
+        String userAccount = "yupi";
+        String userPassword = "";
         String checkPassword = "123456";
-        long result = userService.userRegister(userAccount, userPassword, checkPassword, "234");
-
+        String planetCode = "1";
+        long result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+        Assertions.assertEquals(-1, result);
+        userAccount = "yu";
+        result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+        Assertions.assertEquals(-1, result);
+        userAccount = "yupi";
+        userPassword = "123456";
+        result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+        Assertions.assertEquals(-1, result);
+        userAccount = "yu pi";
+        userPassword = "12345678";
+        result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+        Assertions.assertEquals(-1, result);
+        checkPassword = "123456789";
+        result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+        Assertions.assertEquals(-1, result);
+        userAccount = "dogYupi";
+        checkPassword = "12345678";
+        result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+        Assertions.assertEquals(-1, result);
+        userAccount = "yupi";
+        result = userService.userRegister(userAccount, userPassword, checkPassword, planetCode);
+        Assertions.assertEquals(-1, result);
     }
+
 }
