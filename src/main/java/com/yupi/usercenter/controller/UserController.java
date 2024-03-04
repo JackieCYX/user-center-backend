73f7f076a4498d16bfe8ee050be1cf14c7354694
JordanCYX <2338257659@qq.com>
2024-03-04 19:27:07 +0800

    请求路径修改

--- src/main/java/com/yupi/usercenter/controller/UserController.java
+++ src/main/java/com/yupi/usercenter/controller/UserController.java
@@ -25,7 +25,7 @@ import static com.yupi.usercenter.constant.UserConstant.USER_LOGIN_STATE;
  * 用户接口
  */
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 public class UserController {
 
     @Resource
