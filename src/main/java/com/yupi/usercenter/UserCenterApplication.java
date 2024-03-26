203d4916c6a8f8cd6ee666eae3fe4c93508620f1
JordanCYX <2338257659@qq.com>
2024-03-26 12:24:33 +0800

    启动类添加@EnableRedisHttpSession

--- src/main/java/com/yupi/usercenter/UserCenterApplication.java
+++ src/main/java/com/yupi/usercenter/UserCenterApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @MapperScan("com.yupi.usercenter.mapper")
 @EnableScheduling
+@EnableRedisHttpSession
 public class UserCenterApplication {
 
     public static void main(String[] args) {
